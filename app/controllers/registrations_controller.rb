28e3ff11632721e34de026a266b5f61415842c33
swswiss <stefanachesilviu1993@gmail.com>
2023-12-14 09:21:13 +0200

    waitlisted no refresh

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -101,9 +101,16 @@ class RegistrationsController < ApplicationController
 	def modify_waitlisted_users
 		@registration = Registration.find(params[:id])
 		@tournament = Tournament.find(params[:tournament_id])
-		@user = User.find(@registration.user_id).username
+		@user = User.find(@registration.user_id)
 		@registration.toggle!(:waitlisted)
 		@action = "WaitListed"
+		
+		@players_lvl_beginner = @tournament.registrations.where(level_registration: "level_1", waitlisted: false).pluck(:id, :user_id)
+		@players_lvl_medium = @tournament.registrations.where(level_registration: "level_2", waitlisted: false).pluck(:id, :user_id)
+		@players_lvl_mediumplus = @tournament.registrations.where(level_registration: "level_3", waitlisted: false).pluck(:id, :user_id)
+		@players_lvl_expert = @tournament.registrations.where(level_registration: "level_4", waitlisted: false).pluck(:id, :user_id)
+		@players_lvl_waitlisted = @tournament.registrations.where(waitlisted: true).pluck(:id, :user_id)
+
 		if @registration.waitlisted == false
 			@action = @registration.level_registration
 		end
@@ -117,9 +124,11 @@ class RegistrationsController < ApplicationController
 			@action = "Expert"
 		end
 
-		respond_to do |format|
-			format.turbo_stream
-		end
+		render turbo_stream: 
+			turbo_stream.replace("ceva",
+				partial: "tournaments/player_info",
+				locals: {user: @user, registration: @registration})
+		
 	end
 
 	private
