b0fb91f5ef3c865bbd2df099e244e07153208556
swswiss <stefanachesilviu1993@gmail.com>
2023-12-14 22:15:07 +0200

    generate groups button lazy

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -10,4 +10,12 @@ class Tournament < ApplicationRecord
   validates :max_lvl1, numericality: { only_integer: true }
   validates :max_lvl3, numericality: { only_integer: true }
   validates :max_lvl4, numericality: { only_integer: true }
+
+  def created_at_time
+		if (Time.zone.now - self.created_at) > 1.day
+			self.created_at.strftime("%b %-d")
+		else
+			time_ago_in_words(self.created_at)
+		end
+	end
 end
