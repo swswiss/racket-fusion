26140b71fdf66c52cf4050eebefa7e6e821b1bff
swswiss <stefanachesilviu1993@gmail.com>
2024-01-17 15:42:20 +0200

    blog wip

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,17 +6,28 @@ class BlogsController < ApplicationController
 		@blogs = Blog.all
 	end
 
+	def show
+		@blog = Blog.find(params[:id])
+		
+	end
+
 	def create
 		@blog = Blog.new(blog_params)
-
+	
 		if @blog.save
-			
-					render turbo_stream: 
-					turbo_stream.replace("blog_here",
-						partial: "blogs/blog",
-						locals: {blogs: Blog.all})
-				end
-			end
+			# Successful save
+			redirect_to @blog, notice: 'Blog was successfully created.' # Redirect to the show page for the newly created blog
+		else
+
+		end
+	end
+	
+	def destroy
+		@blog = Blog.find(params[:id])
+		@blog.destroy
+	
+		redirect_to blogs_path, notice: 'Blog was successfully destroyed.'
+	end
 		
 	
 
