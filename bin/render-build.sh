aeb2943178ca6e4f219e7b278fbca1f80b02a2ca
swswiss <stefanachesilviu1993@gmail.com>
2024-02-01 09:28:20 +0200

    new images and checkf reg

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,7 @@ set -o errexit
 bundle install
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
+bundle exec rails runner 'Registration.delete_all'
 bundle exec rails runner 'Tournament.delete_all'
 bundle exec rails db:migrate
 bundle exec rails db:seed
