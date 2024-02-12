ca03f156992d6725d90c4b6c1896fc81cd49441a
swswiss <stefanachesilviu1993@gmail.com>
2024-02-12 10:57:20 +0200

    neimportant

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,7 @@ set -o errexit
 bundle install
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
+bundle exec rails runner 'Blog.delete_all'
 bundle exec rails runner 'Match.delete_all'
 bundle exec rails runner 'Registration.delete_all'
 bundle exec rails runner 'Tournament.delete_all'
