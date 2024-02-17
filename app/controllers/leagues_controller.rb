d9d2d70c840a10331c4065240f3e7937e81db342
swswiss <stefanachesilviu1993@gmail.com>
2024-02-18 01:24:30 +0200

    checks for sign ups

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -12,16 +12,16 @@ class LeaguesController < ApplicationController
 		end,
 		items: 5
 		)
-        city = 'Iasi'
-		api_key = '8391a6cc0b0ad7ddc8ec7fedf25c5a39'
+        # city = 'Iasi'
+		# api_key = '8391a6cc0b0ad7ddc8ec7fedf25c5a39'
 
-		response = HTTParty.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{api_key}&units=metric")
+		# response = HTTParty.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{api_key}&units=metric")
 
-		if response.code == 200
-			weather_data = JSON.parse(response.body)
-			@weather = weather_data['weather'].first["main"].downcase
-		else
-			@weather = "clear"
-		end
+		# if response.code == 200
+		# 	weather_data = JSON.parse(response.body)
+		# 	@weather = weather_data['weather'].first["main"].downcase
+		# else
+		# 	@weather = "clear"
+		# end
     end
 end
