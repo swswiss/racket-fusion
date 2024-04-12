29d683e5b42885c50aab0c93cd8b632cb39759db
swswiss <stefanachesilviu1993@gmail.com>
2024-04-13 02:33:36 +0300

    horizontal sidebar for agent

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,9 +5,6 @@ set -o errexit
 bundle install
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
-bundle exec rails runner 'Blog.delete_all'
-bundle exec rails runner 'Match.delete_all'
-bundle exec rails runner 'Registration.delete_all'
-bundle exec rails runner 'Tournament.delete_all'
+
 bundle exec rails db:migrate
 bundle exec rails db:seed
