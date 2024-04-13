c4839d59555c77c85d395b5311e960464e7356fe
swswiss <stefanachesilviu1993@gmail.com>
2024-04-14 00:35:43 +0300

    nothing

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -5,16 +5,20 @@ class StatisticsController < ApplicationController
 
 	def index
 		@pagy, @matches_for_current_user = pagy(Match.for_current_user(current_user), items: 13)
-
 	end
 
 	def duel
 		@pagy, @matches_for_current_user = pagy(Match.for_current_user(current_user), items: 13)
+		registration_ids = Registration.where(teammate_id: current_user.id).pluck(:id)
+        @all_teammates = Match.where('first_player IN (?) OR second_player IN (?)', registration_ids, registration_ids) || []
 	end
 
 	def headtohead
 		opponent = User.find(params[:user_id])
-		@duels = Match.duels(current_user, opponent)
+		@duels = Match.duels(current_user, opponent) rescue []
+		registration_ids = Registration.where(teammate_id: opponent.id).pluck(:id)
+
+        @all_teammates = Match.where('second_player IN (?)', registration_ids) || []
 	
 		render turbo_stream: 
 			turbo_stream.replace("duels",
